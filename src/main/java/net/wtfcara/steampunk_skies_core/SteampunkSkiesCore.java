8788d7851f6debbe8518c96d88cc2cc559300157
wtfC4ra <wtfC4ra@gmail.com>
2025-08-09 17:36:55 +0100

    Removed Items and Blocks. now just Mixins

--- src/main/java/net/wtfcara/steampunk_skies_core/SteampunkSkiesCore.java
+++ src/main/java/net/wtfcara/steampunk_skies_core/SteampunkSkiesCore.java
@@ -16,9 +16,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.wtfcara.steampunk_skies_core.block.ModBlocks;
-import net.wtfcara.steampunk_skies_core.item.ModItems;
-import net.wtfcara.steampunk_skies_core.item.ModCreativeModeTabs;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -33,9 +30,6 @@ public class SteampunkSkiesCore
     public SteampunkSkiesCore(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
-        ModCreativeModeTabs.register(modEventBus);
-        ModItems.register(modEventBus);
-        ModBlocks.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in
@@ -64,9 +58,7 @@ public class SteampunkSkiesCore
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if(event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS){
-            event.accept(ModBlocks.CRUSHED_BASALT);
-        }
+
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
