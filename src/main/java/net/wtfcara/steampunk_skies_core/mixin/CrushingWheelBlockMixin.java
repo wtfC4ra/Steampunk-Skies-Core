217c975a9d4dbd46af903dca167e92788111493b
wtfC4ra <wtfC4ra@gmail.com>
2025-08-09 16:19:40 +0100

    Working on Crushing Wheel requirement. Inject is working but behaviour is unexpected. all crushing at any speed is cancelled?

---
+++ src/main/java/net/wtfcara/steampunk_skies_core/mixin/CrushingWheelBlockMixin.java
@@ -0,0 +1,17 @@
+package net.wtfcara.steampunk_skies_core.mixin;
+
+import com.simibubi.create.content.kinetics.base.RotatedPillarKineticBlock;
+import com.simibubi.create.content.kinetics.crusher.CrushingWheelBlock;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(CrushingWheelBlock.class)
+public abstract class CrushingWheelBlockMixin extends RotatedPillarKineticBlock {
+    public CrushingWheelBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public SpeedLevel getMinimumRequiredSpeedLevel(){
+        return SpeedLevel.MEDIUM;
+    }
+}
---
+++ src/main/java/net/wtfcara/steampunk_skies_core/mixin/CrushingWheelControllerBlockMixin.java
@@ -0,0 +1,29 @@
+package net.wtfcara.steampunk_skies_core.mixin;
+
+import com.simibubi.create.content.kinetics.crusher.CrushingWheelControllerBlock;
+import com.simibubi.create.content.kinetics.crusher.CrushingWheelControllerBlockEntity;
+import com.simibubi.create.foundation.block.IBE;
+import com.simibubi.create.infrastructure.config.AllConfigs;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CrushingWheelControllerBlock.class)
+public abstract class CrushingWheelControllerBlockMixin implements IBE<CrushingWheelControllerBlockEntity> {
+    @Inject(method = "checkEntityForProcessing", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    private void checkEntityMixin(Level worldIn, BlockPos pos, Entity entityIn, CallbackInfo ci){
+        CrushingWheelControllerBlockEntity be = getBlockEntity(worldIn, pos);
+        System.out.println(be.crushingspeed*50);
+        if (be == null)
+            System.out.println("is null");
+            ci.cancel();
+        if (be.crushingspeed*50 < AllConfigs.server().kinetics.mediumSpeed.get()){
+            System.out.println("Wheels not fast enough");
+            ci.cancel();
+        }
+    }
+}
