8f735df097d4488c255ac3d9058d26183eafeeb4
wtfC4ra <wtfC4ra@gmail.com>
2025-08-09 17:35:36 +0100

    Disabled Blaze Burners working for SteamPowered boilers.

---
+++ src/main/java/net/wtfcara/steampunk_skies_core/mixin/BoilerHeatersMixin.java
@@ -0,0 +1,15 @@
+package net.wtfcara.steampunk_skies_core.mixin;
+
+import com.simibubi.create.content.fluids.tank.BoilerHeaters;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BoilerHeaters.class)
+public abstract class BoilerHeatersMixin {
+    @Inject(method = "registerDefaults", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    private static void registerDefaults(CallbackInfo ci){
+        ci.cancel();
+    }
+}
